b65fcb4d09226fa6e6208c775df51ef5a00a6b8d
Saleh Masum <salehmasum@gmail.com>
2022-07-03 17:30:26 +1000

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Saleh Masum on 3/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
